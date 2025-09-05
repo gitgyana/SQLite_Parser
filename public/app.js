f4786db2f229fc1297b7e870cbf896a98911f5b9
Gyana Priyadarshi <gyana.career@gmail.com>
2025-09-05 22:53:10 +0530

    Update UI behavior on file select

--- public/app.js
+++ public/app.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dbFileInput = document.getElementById('dbFile');
     const fileNameSpan = document.getElementById('file-label');
+    const tableSelect = document.querySelector('label[for="tableSelect"]');
+    const selectElement = document.getElementById('tableSelect');
+    const option = selectElement.querySelector('option[value="ALL"]');
+
+    tableSelect.style.display = 'none';
+    selectElement.style.display = 'none';
+    option.style.display = 'none';
 
     dbFileInput.addEventListener('change', () => {
         fileNameSpan.textContent = dbFileInput.files[0] ? dbFileInput.files[0].name : 'No file chosen';
+
+        tableSelect.style.display = 'block';
+        selectElement.style.display = 'block';
+        option.style.display = 'block';
     });
 });
